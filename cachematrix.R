f59c9913645f851b27194a64b74af786794b6a4f
hcil <hhchiang00@gmail.com>
2015-06-21 02:07:50 -0500

    modified cachematrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This function, named "makeCacheMatrix", creates a special "matrix" object that 
+# can cache its inverse.
 
-## Write a short comment describing this function
+# This function is really a list containing a function to
+# set the value of the matrix, get the value of the matrix
+# set the value of the inverse, and get the value of the inverse
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) { 
+  invrs <- NULL 
+  
+  # set the value of the matrix
+  set <- function(y) { 
+    x <<- y 
+    invrs <<- NULL 
+  } 
+  
+  # get the value of the matrix
+  get <- function() x 
+  
+  # set the vaule of the inverse
+  setinverse <- function(inverse) invrs <<- inverse 
+  
+  # get the value of the inverse
+  getinverse <- function() invrs
+  
+  # return a list of the above functions
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse) 
+} 
 
-}
 
 
-## Write a short comment describing this function
+# The following function calculates the inverse of the special "matrix" created 
+# with the above function. 
+# It first checks to see if the inverse has already been calculated. If so, 
+# it gets the inverse from the cache and skips the computation. Otherwise, 
+# it calculates the inverse of the matrix and sets the value of the inverse in the
+# cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  
+  # check if inverse is already cached. If so, get the inverse from the cache.
+  invrs <- x$getinverse() 
+  if(!is.null(invrs)) { 
+    message("getting cached data.") 
+    return(invrs) 
+  } 
+  
+  # else, get the matrix and calculate the inverse.
+  data <- x$get() 
+  invrs <- solve(data, ...) 
+  
+  # cache the inverse of the matrix and return the result.
+  x$setinverse(invrs) 
+  invrs 
+} 
+
+
